eb692371e7a6a910007e2367987686d6a58eb0c3
OngTronnnng <116233986+OngTronnnng@users.noreply.github.com>
2024-10-02 12:48:18 +0700

    Create install_story.sh

---
+++ install_story.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# Update and install dependencies
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+
+# Install Go, if needed
+cd $HOME
+VER="1.23.1"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+# Download Geth binaries
+cd $HOME
+rm -rf bin
+mkdir bin
+cd bin
+wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.3-b224fdf.tar.gz
+tar -xvzf geth-linux-amd64-0.9.3-b224fdf.tar.gz
+mv ~/bin/geth-linux-amd64-0.9.3-b224fdf/geth ~/go/bin/
+mkdir -p ~/.story/story
+mkdir -p ~/.story/geth
+
+# Install Story
+cd $HOME
+rm -rf story
+git clone https://github.com/piplabs/story
+cd story
+git checkout v0.10.1
+go build -o story ./client
+sudo mv ~/story/story ~/go/bin/
+
+# Initialize the Story client
+story init --moniker test --network iliad
+
+# Create Geth service file
+sudo tee /etc/systemd/system/story-geth.service > /dev/null <<EOF
+[Unit]
+Description=Story Geth daemon
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which geth) --iliad --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 0.0.0.0 --http.port 8545 --ws --ws.api eth,web3,net,txpool --ws.addr 0.0.0.0 --ws.port 8546
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Create Story service file
+sudo tee /etc/systemd/system/story.service > /dev/null <<EOF
+[Unit]
+Description=Story Service
+After=network.target
+
+[Service]
+User=$USER
+WorkingDirectory=$HOME/.story/story
+ExecStart=$(which story) run
+
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable and start Geth service
+sudo systemctl daemon-reload
+sudo systemctl enable story-geth
+sudo systemctl restart story-geth && sudo journalctl -u story-geth -f
+
+# Enable and start Story service
+sudo systemctl daemon-reload
+sudo systemctl enable story
+sudo systemctl restart story && sudo journalctl -u story -f
